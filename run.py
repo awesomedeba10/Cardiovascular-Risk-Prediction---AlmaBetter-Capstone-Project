c40fa4c421746fd983973b0634837e3483107a36
awesomedeba10 <officialdeba10@gmail.com>
2022-07-28 20:34:01 +0530

    heroku deployment changes v1.5

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import app
 import os
 
 app.run(host='0.0.0.0',
-    port=app.config.get('APP_PORT', int(os.environ.get('PORT', 5000))),
+    port=int(os.environ.get('PORT', 5000)),
     debug=app.config.get('APP_DEBUG', False))
