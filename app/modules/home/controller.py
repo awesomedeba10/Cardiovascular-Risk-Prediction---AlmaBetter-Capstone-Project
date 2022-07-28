5ef5f35a0e7d8194466e3a7b020092a122d7427d
awesomedeba10 <officialdeba10@gmail.com>
2022-07-29 01:58:05 +0530

    added content v1.0

--- app/modules/home/controller.py
+++ app/modules/home/controller.py
@@ -8,3 +8,12 @@ home_blueprint = Blueprint('home', __name__)
 @home_blueprint.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@home_blueprint.route('/risk-prediction', methods=['GET'])
+def prediction():
+    return render_template('prediction.html')
+
+@home_blueprint.route('/risk-prediction-result', methods=['POST'])
+def prediction_result():
+    data = request.form
+    print(data)
