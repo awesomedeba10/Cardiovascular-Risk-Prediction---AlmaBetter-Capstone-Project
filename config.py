8d6007ee709558e41e281d79281396b9bacbc39b
awesomedeba10 <officialdeba10@gmail.com>
2022-07-28 23:00:23 +0530

    heroku deployment changes v1.8

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+import os
+
 APP_ENV = 'Development'
-APP_HOST = '127.0.0.1'
-APP_PORT = 8080
+APP_HOST = '0.0.0.0'
+APP_PORT = os.getenv('PORT') or 5000 # if local env is not set
 APP_DEBUG = True
 
 # Define the application directory
-import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 MODEL_DIR = os.path.join(BASE_DIR, 'models')
 DATA_DIR = os.path.join(BASE_DIR, 'data')
--- run.py
+++ run.py
@@ -2,4 +2,6 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=app.config.get('APP_HOST', '127.0.0.1'),
+    port=int(app.config.get('APP_PORT', 5000)),
+    debug=app.config.get('APP_DEBUG', False))
